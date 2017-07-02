6b3eb12632d673f52f633467193afe761c7bef23
Sai Vishal <thug@droidthug.me>
2017-07-02 12:40:51 +0000

    a3: import audio configs
    Signed-off-by: Sai Vishal <thug@droidthug.me>

--- device.mk
+++ device.mk
@@ -78,10 +78,13 @@ PRODUCT_PACKAGES += \
     tinymix
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/etc/audio_effects.conf:system/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/etc/audio_platform_info.xml:system/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/etc/audio_platform_info_extcodec.xml:system/etc/audio_platform_info_extcodec.xml \
+    $(LOCAL_PATH)/audio/etc/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/etc/mixer_paths.xml:system/etc/mixer_paths.xml \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
-    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf
 
 # Display
 PRODUCT_PACKAGES += \
