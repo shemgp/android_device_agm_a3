53bde0d14c3c685b240976da849566e2a34d2e35
Sai Vishal <thug@droidthug.me>
2017-07-02 12:04:50 +0000

    a3: initial Keylayout
    Signed-off-by: Sai Vishal <thug@droidthug.me>

--- device.mk
+++ device.mk
@@ -109,6 +109,12 @@ PRODUCT_PACKAGES += \
     ueventd.qcom.rc
 
 # Input
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/ft5x06_ts.kl \
+    $(LOCAL_PATH)/keylayout/fpc1020.kl:system/usr/keylayout/fpc1020.kl \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/synaptics_dsx.kl:system/usr/keylayout/synaptics_dsx.kl \
+    $(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl
 
 # IRQ
 PRODUCT_COPY_FILES += \
